46082b84404570be0c7245565779b89254786605
Eduardo26 <jguevaral@uni.pe>
2023-04-26 11:19:51 -0500

    create api order

---
+++ pages/api/orders/index.js
@@ -0,0 +1,22 @@
+import Order from "../../../models/Order";
+import db from "../../../utils/db";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+
+const handler = async (req, res) => {
+  const user = await getServerSession(req, res, authOptions);
+
+  if (!user) {
+    return res.status(401).send("signin required");
+  }
+
+  await db.connect();
+  const newOrder = new Order({
+    ...req.body,
+    user: user.user._id,
+  });
+
+  const order = await newOrder.save();
+  res.status(201).send(order);
+};
+export default handler;
